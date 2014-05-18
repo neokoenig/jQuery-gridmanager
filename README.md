75edc51781a32e3371156f68de562df648d122fa
Tom King <tom@oxalto.co.uk>
2014-05-18 08:46:08 +0100

    Initial Commit

---
+++ README.md
@@ -0,0 +1,31 @@
+# jQuery GridManager
+
+A way of building rows and grids with built in editable regions
+
+## Getting Started
+
+Download the [production version][min] or the [development version][max].
+
+[min]: https://raw.github.com/neokeonig/jquery-gridmanager/master/dist/jquery.gridmanager.min.js
+[max]: https://raw.github.com/neokeonig/jquery-gridmanager/master/dist/jquery.gridmanager.js
+
+In your web page:
+
+```html
+<script src="jquery.js"></script>
+<script src="dist/gridmanager.min.js"></script>
+<script>
+jQuery(function($) {
+  $.awesome(); // "awesome"
+});
+</script>
+```
+
+## Documentation
+_(Coming soon)_
+
+## Examples
+_(Coming soon)_
+
+## Release History
+_(Nothing yet)_
---
+++ src/gridmanager.js
@@ -0,0 +1,38 @@
+/*
+ * GridManager
+ * 
+ *
+ * Copyright (c) 2014 Tom King
+ * Licensed under the MIT license.
+ */
+
+(function ($) {
+
+  // Collection method.
+  $.fn.awesome = function () {
+    return this.each(function (i) {
+      // Do something awesome to each selected element.
+      $(this).html('awesome' + i);
+    });
+  };
+
+  // Static method.
+  $.awesome = function (options) {
+    // Override default options with passed-in options.
+    options = $.extend({}, $.awesome.options, options);
+    // Return something awesome.
+    return 'awesome' + options.punctuation;
+  };
+
+  // Static method default options.
+  $.awesome.options = {
+    punctuation: '.'
+  };
+
+  // Custom selector.
+  $.expr[':'].awesome = function (elem) {
+    // Is this element awesome?
+    return $(elem).text().indexOf('awesome') !== -1;
+  };
+
+}(jQuery));
