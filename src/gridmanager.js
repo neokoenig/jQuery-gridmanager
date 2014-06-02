3df5464b3f60c41398f590ff4d8f18ba8b66e09e
Tom King <tom@oxalto.co.uk>
2014-06-02 17:12:30 +0100

    Different buttons, HTML edit mode

--- src/gridmanager.js
+++ src/gridmanager.js
@@ -59,8 +59,7 @@
          gm.$el.prepend(
               $('<div/>', 
                   {'id': gm.options.controlId, 'class': gm.options.gmClearClass }
-              ).prepend(
-                  gm.options.canvasModal, 
+              ).prepend( 
                     $('<div/>', {"class": gm.options.rowClass}).html(
                        $('<div/>', {"class": gm.options.colClass + 12}).addClass(gm.options.colAdditionalClass).html(
                           $('<div/>', {'id': 'gm-addnew'})
@@ -82,12 +81,32 @@
            gm.log("+ InitControls Running");    
 
            // Turn editing on or off 
-           gm.$el.on("click", ".gm-switch", function(){ 
+           gm.$el.on("click", ".gm-preview", function(){ 
                if(gm.status){ 
                 gm.deinitCanvas();
+                 $(this).parent().find(".gm-mode").prop('disabled', true);
               } else { 
-                gm.initCanvas(); 
+                gm.initCanvas();   
+                 $(this).parent().find(".gm-mode").prop('disabled', false);
               }
+              $(this).toggleClass(gm.options.gmDangerClass);
+
+          // Switch editing mode
+            }).on("click", ".gm-mode", function(){ 
+              if(gm.mode === "visual"){ 
+                 gm.deinitCanvas(); 
+                 canvas.html($('<textarea/>').attr("cols", 130).attr("rows", 25).val(canvas.html()));
+                 gm.mode="html";
+                 $(this).parent().find(".gm-preview").prop('disabled', true);
+                 gm.log($(this));
+              } else {  
+                var editedSource=canvas.find("textarea").val();
+                 canvas.html(editedSource);
+                 gm.initCanvas();
+                 gm.mode="visual"; 
+                 $(this).parent().find(".gm-preview").prop('disabled', false);
+              } 
+              $(this).toggleClass(gm.options.gmDangerClass);
 
             // Make region editable
             }).on("click", ".gm-editholder", function(){ 
@@ -100,18 +119,8 @@
             // Save Function
             }).on("click", "a.gm-save", function(){ 
                 gm.deinitCanvas();
-                gm.saveremote(); 
-
-            // View Source in Alert Dialog
-            // NB not working in foundation yet.
-            }).on("click", "a.gm-viewsource", function(){  
-                gm.deinitCanvas();  
-                var source=gm.htmlEncode(canvas.html()); 
-                var modal=$("#canvasModal"); 
-                    modal.find(".modal-body").html(
-                       $('<pre/>', {"class": 'pre-scrollable'}).html(source)
-                    ); 
-                    modal.modal(); 
+                gm.saveremote();  
+                    
             /* Row settings */
             }).on("click", "a.gm-rowSettings", function(){ 
                  var row=$(this).closest(gm.options.rowSelector); 
@@ -171,7 +180,7 @@
                $(this).closest("." +gm.options.gmEditClass).animate({opacity: 'hide', height: 'hide'}, 400, function(){this.remove();});  
 
             // For all the above, prevent default.
-            }).on("click", "a.gm-resetgrid, a.gm-remove, a.gm-save, button.gm-switch, a.gm-viewsource, a.gm-addColumn, a.gm-colDecrease, a.gm-colIncrease", function(e){ 
+            }).on("click", "a.gm-resetgrid, a.gm-remove, a.gm-save, button.gm-preview, a.gm-viewsource, a.gm-addColumn, a.gm-colDecrease, a.gm-colIncrease", function(e){ 
                gm.log("Clicked: "   + $.grep((this).className.split(" "), function(v){
                  return v.indexOf('gm-') === 0;
              }).join()); 
@@ -230,7 +239,8 @@
                 tolerance: "pointer",
                 cursor: "move"
               });  
-            gm.status=true; 
+            gm.status=true;
+            gm.mode="visual"; 
         };
 
         /*
@@ -595,8 +605,6 @@
         // Canvas ID
         canvasId: "gm-canvas",
 
-        // Shortcut for modal window markup
-        canvasModal: "<div id='canvasModal' class='modal fade' tabindex='-1' role='dialog' aria-labelledby='canvasModal' aria-hidden='true'><div class='modal-dialog modal-lg'><div class='modal-content'><div class='modal-header'><button type='button' class='close' data-dismiss='modal' aria-hidden='true'>&times;</button><h4 class='modal-title' id='myModalLabel'>GridManager</h4></div><div class='modal-body'></div></div></div></div></div>",        
   /*
      Control Bar---------------
   */
@@ -613,7 +621,7 @@
         controlButtonSpanClass: "glyphicon glyphicon-plus-sign",
 
         // Control bar RH dropdown markup
-        controlAppend: "<div class='btn-group pull-right'><button type='button' class='btn btn-xs btn-primary gm-switch'><span class='glyphicon glyphicon-off'></span> Editor</button><button type='button' class='btn  btn-xs  btn-primary dropdown-toggle' data-toggle='dropdown'><span class='caret'></span><span class='sr-only'>Toggle Dropdown</span></button><ul class='dropdown-menu' role='menu'><li><a title='Save'  href='#' class='gm-save'><span class='glyphicon glyphicon-ok'></span> Save</a></li><li><a title='View Source' href='#' class='gm-viewsource'><span class='glyphicon glyphicon-zoom-in'></span> View Source</a></li><li><a title='Reset Grid' href='#' class='gm-resetgrid'><span class='glyphicon glyphicon-trash'></span> Reset</a></li></ul></div>",
+        controlAppend: "<div class='btn-group pull-right'><button title='Edit Source Code' type='button' class='btn btn-xs btn-primary gm-mode'><span class='glyphicon glyphicon-chevron-left'></span><span class='glyphicon glyphicon-chevron-right'></span></button><button title='Preview' type='button' class='btn btn-xs btn-primary gm-preview'><span class='glyphicon glyphicon-eye-open'></span></button><button type='button' class='btn  btn-xs  btn-primary dropdown-toggle' data-toggle='dropdown'><span class='caret'></span><span class='sr-only'>Toggle Dropdown</span></button><ul class='dropdown-menu' role='menu'><li><a title='Save'  href='#' class='gm-save'><span class='glyphicon glyphicon-ok'></span> Save</a></li><li><a title='View Source' href='#' class='gm-viewsource'><span class='glyphicon glyphicon-zoom-in'></span> Edit Source</a></li><li><a title='Reset Grid' href='#' class='gm-resetgrid'><span class='glyphicon glyphicon-trash'></span> Reset</a></li></ul></div>",
    /*
      General editing classes---------------
   */      
