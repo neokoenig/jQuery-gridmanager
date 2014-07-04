75c4b165b110c5b9eb4ab31001993a8c7c877a13
Percy D Brea <pbreah@gmail.com>
2014-07-04 16:58:17 -0400

    Fixed missing title attribute on buttons and we now check if it is not set, and if so set a empty value

--- src/gridmanager.js
+++ src/gridmanager.js
@@ -366,15 +366,19 @@
                 if(typeof curr_control.iconClass === 'undefined') {
                   curr_control.iconClass = 'fa fa-file-code-o';
                 }
-                if(typeof curr_control.btnLabel == 'undefined') {
+                if(typeof curr_control.btnLabel === 'undefined') {
                   curr_control.btnLabel = '';
+                }
+                if(typeof curr_control.title === 'undefined') {
+                  curr_control.title = '';
                 }
 
                 btnObj = {
                   element: 'a',
                   btnClass: 'gm-'+curr_control.callback,
                   iconClass: curr_control.iconClass,
-                  btnLabel: curr_control.btnLabel
+                  btnLabel: curr_control.btnLabel,
+                  title: curr_control.title
                 };
 
                 $.each(elems, function(i, current_elem) {
@@ -787,6 +791,7 @@
           var buttons=[];
           $.each(btns, function(i, val){  
             val.btnLabel = (typeof val.btnLabel === 'undefined')? '' : val.btnLabel;
+            val.title = (typeof val.title === 'undefined')? '' : val.title;
             buttons.push("<" + val.element +" title='" + val.title + "' class='" + val.btnClass + "'><span class='"+val.iconClass+"'></span>&nbsp;" + val.btnLabel + "</" + val.element + "> ");
           }); 
           return buttons.join("");
