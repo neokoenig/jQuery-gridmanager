d0a596a503ed9a98b84ec698fed50306ebdb8941
Tom King <admin@oxalto.co.uk>
2014-09-14 11:28:32 +0100

    New defaultContent placeholder
    Made this an option for upcoming internationalisation

--- src/gridmanager.js
+++ src/gridmanager.js
@@ -977,7 +977,7 @@
           var cTagOpen = '<!--'+gm.options.gmEditRegion+'-->',
               cTagClose = '<!--\/'+gm.options.gmEditRegion+'-->',
               elem = null;
-              html = html || '<p>New Content</p>';
+              html = html || gm.options.gmContentPlaceholder;
               btn = null;
           $(('.'+gm.options.gmToolClass+':last'),container)
           .before(elem = $('<div>').addClass(gm.options.gmEditRegion+' '+gm.options.contentDraggableClass)
@@ -1624,6 +1624,9 @@
 
         // Column resizing +- value: this is also the colMin value, as columns won't be able to go smaller than this number (otherwise you hit zero and all hell breaks loose)
         colResizeStep: 1,
+
+        // Default content placeholder
+        gmContentPlaceholder: "<p>New Content</p>",
 
   /*
      Rich Text Editors---------------
